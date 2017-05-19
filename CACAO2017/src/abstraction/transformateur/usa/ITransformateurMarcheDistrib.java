21dd8ce23e2b3606826920b25a5d0bc5fcc3868f
Tabsissi <mehdi_tabsissi10@hotmail.com>
2017-05-19 14:42:44 +0200

    Interfaces marché(s)

--- CACAO2017/src/abstraction/transformateur/usa/ITransformateurMarcheDistrib.java
+++ CACAO2017/src/abstraction/transformateur/usa/ITransformateurMarcheDistrib.java
@@ -1,5 +1,11 @@
 package abstraction.transformateur.usa;
 
 public interface ITransformateurMarcheDistrib {
+	
+	public double getprixMin();
+	
+	public void notificationvente(double prix,double quantité);
+	
+	
 
 }
--- CACAO2017/src/abstraction/transformateur/usa/ITransformateurMarcheProducteur.java
+++ CACAO2017/src/abstraction/transformateur/usa/ITransformateurMarcheProducteur.java
@@ -2,4 +2,10 @@ package abstraction.transformateur.usa;
 
 public interface ITransformateurMarcheProducteur {
 
+	
+	public double QteSouhaite();
+	
+	public void notificationAchat(double quantite, double prix);
+	
+	public int hashCode();
 }
