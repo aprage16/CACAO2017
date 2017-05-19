3159fe15f28c3c6eaf2adafde8be4ac7d5133648
eveline boucher <eveline boucher@roklin>
2017-05-19 16:41:55 +0200

    prod modif

--- CACAO2017/src/abstraction/producteur/cotedivoire/ProductionCoteDIvoire.java
+++ CACAO2017/src/abstraction/producteur/cotedivoire/ProductionCoteDIvoire.java
@@ -15,32 +15,27 @@ public class ProductionCoteDIvoire implements Acteur, IProducteur{
 	public static final double VARIATIONALEATOIREPRODUCTION = 0.01; 
 	
 	private int  production; //Liste des productions par périodes
-	private Stock stock;          // Représente notre stock 
+	private Stock stock;          // Represente notre stock 
 	private Treso tresorerie;     // Représente notre trésorerie
-	private Indicateur productionIndicateur;	// Indicateur de production 
-	private Indicateur stockIndicateur;		// Indicateur du stock
-	private Indicateur tresoIndicateur;		// Indicateur de tréso
-	private Indicateur vente;		// Indicateur de vente
-	private Journal journal;		// Journal de Production Côte d'Ivoire
-	
-	//Cf Marché
+	private Indicateur productionIndicateur;
+	private Indicateur stockIndicateur;
+	private Indicateur tresoIndicateur;
+	private Indicateur vente;
+	//Cf marché
 	public int hashCode() {
 		return this.getNom().hashCode();
 	}
 	
-	//Constructeur Production Côte d'Ivoire
+	//Constructeur Production cote d'ivoire
 	public ProductionCoteDIvoire(int prods, Stock stock, Treso treso){ 
 		this.production = prods; 
 		this.stock=stock;
 		this.tresorerie = treso; 
 	}
-	
 	public ProductionCoteDIvoire() {
-		this.production = 0;		//Initialisation de la production
-		this.stock= new Stock(0);		//Initialisation du Stock
-		this.tresorerie= new Treso();		//Initialisation de la trésorerie
-		
-		//Création des indicateurs Production Côte d'Ivoire
+		this.production = 0;
+		this.stock= new Stock(0);
+		this.tresorerie= new Treso();
 		this.productionIndicateur=new Indicateur("6_PROD_COT_production",this,0.0);
 		Monde.LE_MONDE.ajouterIndicateur( this.productionIndicateur );
 		this.stockIndicateur = new Indicateur("6_PROD_COT_stock",this,0.0);
@@ -49,10 +44,6 @@ public class ProductionCoteDIvoire implements Acteur, IProducteur{
 		Monde.LE_MONDE.ajouterIndicateur(this.tresoIndicateur);
 		this.vente= new Indicateur("6_PROD_COT_vente",this,0.0);
 		Monde.LE_MONDE.ajouterIndicateur(this.vente);
-		
-		//Création du Journal Production Côte d'Ivoire
-		this.journal = new Journal("Journal de "+ getNom());
-		Monde.LE_MONDE.ajouterJournal(this.journal);
 		
 	}
 
@@ -69,22 +60,20 @@ public class ProductionCoteDIvoire implements Acteur, IProducteur{
 		double prod_max = PRODUCTIONMOYENNE + (double)(PRODUCTIONMOYENNE*VARIATIONALEATOIREPRODUCTION);
 		double prod = prod_min + (double)Math.random()*(prod_max - prod_min); // Production random entre prod_min et prod_max
 		this.production=(int)prod; // ajout dans la liste de production
-		this.stock.addStock((int)prod); //Ajout dans le stock
-		this.productionIndicateur.setValeur(this, (int)prod); //Ajout de la production dans l'indicateur de production
-		this.journal.ajouter(" Valeur Produite: "+ (int) prod +"à l'étape du Monde: "+Monde.LE_MONDE.getStep()); // Ajout de la production dans le Journal
+		this.stock.addStock((int)prod);
+		this.productionIndicateur.setValeur(this, (int)prod);
 	}
 	
 	//Accesseur Nom
 	public String getNom() {
 		return "Production Cote d'Ivoire"; 
 	}
-	
-	//Quantité mise en vente
-	public double quantiteMiseEnvente() {   
+
+	public double quantiteMiseEnvente() {   // correspond a la quantité mise en vente//
 		return this.stock.getStock(); 
 	}
 
-	//Notification Vente
+
 	public void notificationVente(double quantite, double coursActuel) {	// grace a la notification de vente on met a jour // 
 		this.vente.setValeur(this,quantite);
 		this.stock.addStock(-quantite);
@@ -92,6 +81,7 @@ public class ProductionCoteDIvoire implements Acteur, IProducteur{
 	}
 	
 	//NEXT "Centre du programme -> Passage à la période suivante" 
+	
 	public void next() {
 		this.variationProduction();
 		this.stockIndicateur.setValeur(this,this.stock.getStock());
