3d4af1ca30a171286e43870bf4484344a50d6065
lolotteisyoung <lptacite@gmail.com>
2017-06-02 14:54:12 +0200

    prise en compte intempéries

--- CACAO2017/src/abstraction/producteur/ameriquelatine/Recolte.java
+++ CACAO2017/src/abstraction/producteur/ameriquelatine/Recolte.java
@@ -1,4 +1,5 @@
 package abstraction.producteur.ameriquelatine;
+import  java.util.Random;
 
 import abstraction.distributeur.europe.MondeV1;
 
@@ -10,23 +11,31 @@ public class Recolte {
 	private double indiceRecolte; // Indicateur variant entre 0 et 1 : pourcentage pour fixer nb de fèves totales
 	public final static double INDICE_REEL = 1.54; // Ajustement de l'indice pour atteindre la production annuel
 	
+
+	
 	public Recolte(double indiceRecolte){// indice initial
 		this.indiceRecolte=indiceRecolte;
 	}
 
 	public int getQterecoltee() {
 		return (int)(SURFACE_CULTIVABLE*indiceRecolte);
-
 	}
 
+	public int intemperies(){
+		Random rnd = new Random();
+		return rnd.nextInt(52);
+	}
+	
 	public void miseAJourIndice() {
-	//* une année = 26 next
-		int periode = MondeV1.LE_MONDE.getStep();
-		double[] indice = {0.6,0.6,0.6,0.6,0.5,0.7,0.7,0.6,0.5,0.5,0.4,0.4,0.3,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.9,0.8,0.8,0.7,0.6,0.6};
-		this.indiceRecolte= indice[periode%26]*INDICE_REEL+Math.random()*0.1;
+		//* une année = 26 next
+		if(intemperies()==45){
+			this.indiceRecolte=0;
+		}else{
+			int periode = MondeV1.LE_MONDE.getStep();
+			double[] indice = {0.6,0.6,0.6,0.6,0.5,0.7,0.7,0.6,0.5,0.5,0.4,0.4,0.3,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.9,0.8,0.8,0.7,0.6,0.6};
+			this.indiceRecolte= indice[periode%26]*INDICE_REEL+Math.random()*0.1;
+		}
 	}
-	
-	
 		
-	
 }
+
