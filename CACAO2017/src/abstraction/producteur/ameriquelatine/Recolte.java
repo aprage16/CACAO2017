b91c3d6d5d4a85d481b6f98ece3cdb7630470037
lolotteisyoung <lptacite@gmail.com>
2017-06-02 14:28:11 +0200

    MiseAJourIndice

--- CACAO2017/src/abstraction/producteur/ameriquelatine/Recolte.java
+++ CACAO2017/src/abstraction/producteur/ameriquelatine/Recolte.java
@@ -8,6 +8,7 @@ import abstraction.distributeur.europe.MondeV1;
 public class Recolte {
 	public final static int SURFACE_CULTIVABLE=30000; //Constante : surface cultivable max en hectare
 	private double indiceRecolte; // Indicateur variant entre 0 et 1 : pourcentage pour fixer nb de fèves totales
+	public final static double INDICE_REEL = 1.54; // Ajustement de l'indice pour atteindre la production annuel
 	
 	public Recolte(double indiceRecolte){// indice initial
 		this.indiceRecolte=indiceRecolte;
@@ -22,8 +23,9 @@ public class Recolte {
 	//* une année = 26 next
 		int periode = MondeV1.LE_MONDE.getStep();
 		double[] indice = {0.6,0.6,0.6,0.6,0.5,0.7,0.7,0.6,0.5,0.5,0.4,0.4,0.3,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.9,0.8,0.8,0.7,0.6,0.6};
-		this.indiceRecolte= indice[periode%26]+Math.random()*0.1;
+		this.indiceRecolte= indice[periode%26]*INDICE_REEL+Math.random()*0.1;
 	}
+	
 	
 		
 	
