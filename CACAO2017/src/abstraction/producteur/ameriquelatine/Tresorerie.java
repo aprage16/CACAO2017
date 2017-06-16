1423ce37719703b95bb61aaa6b4d6e2c54561603
lolotteisyoung <lptacite@gmail.com>
2017-06-16 16:16:30 +0200

    treso

--- CACAO2017/src/abstraction/producteur/ameriquelatine/Tresorerie.java
+++ CACAO2017/src/abstraction/producteur/ameriquelatine/Tresorerie.java
@@ -11,7 +11,7 @@ import abstraction.fourni.Indicateur;
 public class Tresorerie {
 	private Acteur act;
 	public final static int CHARGESPROD=5000000;// ($) charges fixes (hors coût de stock)
-	public final static double COUTSTOCK=500; // (plutôt 500~1000 dans la réalité)
+	public final static double COUTSTOCK=0; // (plutôt 500~1000 dans la réalité)
 	public final static double COUTSALARIE=47400 ; //  prix d'un salarie (recolte) par tonne de cacao récolté et par an
 	private Indicateur tresorerie; // argent en banque
 	private Stock stock; 
