8002a4ad2868a80b71f5e512742263f217ffe845
wowalid <walidarbel77@gmail.com>
2017-06-28 11:12:39 +0200

    test

--- CACAO2017/src/abstraction/producteur/ameriquelatine/Recolte.java
+++ CACAO2017/src/abstraction/producteur/ameriquelatine/Recolte.java
@@ -8,37 +8,35 @@ import abstraction.distributeur.europe.MondeV1;
 //26/04 Adrien
 
 public class Recolte {
-	public final static int SURFACE_CULTIVABLE=35000; //(realite 30000) Constante : surface cultivable max en hectare
+	
+	public Producteur prod;
+	public double surfaceCultivable; //(realite 30000) Constante : surface cultivable max en hectare
 	private double indiceRecolte; // Indicateur variant entre 0 et 1 : pourcentage pour fixer nb de fèves totales
 	
-	public Recolte(double indiceRecolte){// indice initial
+	public Recolte(double indiceRecolte, Producteur prod){// indice initial
 		this.indiceRecolte=indiceRecolte;
+		this.surfaceCultivable=30000;
+		this.prod=prod;
 	}
 
 	public int getQterecoltee() {
-		return (int)(SURFACE_CULTIVABLE*indiceRecolte);
+		return (int)(this.getSurfaceCultivable()*indiceRecolte);
 	}
 	
-	public int intemperie(){
-		return (int) (Math.random()*52);
+	public double getSurfaceCultivable(){
+		return this.surfaceCultivable;
 	}
 	
-	public void miseAJourIndice() {
-	//* une année = 26 next
-		if(intemperie()==50){
-			this.indiceRecolte=0;
-		}else{
-			int periode = MondeV1.LE_MONDE.getStep();
-			double[] indice = {0.6,0.6,0.6,0.6,0.5,0.7,0.7,0.6,0.5,0.5,0.4,0.4,0.3,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.9,0.8,0.8,0.7,0.6,0.6};
-			this.indiceRecolte= indice[periode%26]+Math.random()*0.1;
+	public void setSurfaceCultivable(double quantite){
+		if(prod.getQteVendue()/quantite>=0.9 && this.getSurfaceCultivable()<50000){
+			this.surfaceCultivable+=1000;
 		}
-	}
-	
+		if(prod.getQteVendue()/quantite<0.5 && this.getSurfaceCultivable()>20000){
+			this.surfaceCultivable-=1000;
+		}
 		
-	
-}
-
-
+	}
+	public int intemperie(){
 		return (int) (Math.random()*52);
 	}
 	
