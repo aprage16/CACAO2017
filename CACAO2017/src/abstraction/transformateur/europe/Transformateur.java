7c06fba0c4e0f455c136d78fcb300e047700d813
theostourm <t.stourm@gmail.com>
2017-06-28 09:53:16 +0200

    commit qui sert a rien

--- CACAO2017/src/abstraction/transformateur/europe/Transformateur.java
+++ CACAO2017/src/abstraction/transformateur/europe/Transformateur.java
@@ -264,11 +264,8 @@ public class Transformateur implements ITransformateurMarcheDistrib, Acteur,ICon
 	
 	private List<Devis> l;
 	
-<<<<<<< HEAD
 
-=======
 	@Override
->>>>>>> CACAO2017/master
 	public void envoieDevis(Devis d) { //récupère la liste des différents devis
 		this.l.add(d);
 	}
