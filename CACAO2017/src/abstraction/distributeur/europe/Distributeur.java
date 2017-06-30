196f70e21668936b31796b664818c71650f8c3cb
danjud <dan.judkiewicz@gmail.com>
2017-06-30 15:20:24 +0200

    Revert "un commit"
    This reverts commit d6db6224e0419203c910d52d0bc8ac83b793af93.
    
    # Conflicts:
    #	CACAO2017/src/abstraction/distributeur/amerique/DistributeurUS.java

--- CACAO2017/src/abstraction/distributeur/europe/Distributeur.java
+++ CACAO2017/src/abstraction/distributeur/europe/Distributeur.java
@@ -3,7 +3,6 @@
 package abstraction.distributeur.europe;
 import abstraction.fourni.v0.*;
 import abstraction.distributeur.amerique.DistribClient;
-import abstraction.distributeur.amerique.DistributeurUS;
 import abstraction.fourni.*;
 import java.util.ArrayList;
 import java.util.List;
